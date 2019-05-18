102972e3c1938408fb2c41f575d038f3ef40b5f1
Mariak Achuoth <achuothmariak@gmail.com>
2019-05-17 23:08:10 -0400

    edgeinsets set for the stackviews

--- AppStore/Controller/searchResultCell.swift
+++ AppStore/Controller/searchResultCell.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class searchResultCell: UICollectionViewCell {
     
-    let imageView: UIImageView = {
+    let appIconImagView: UIImageView = {
         let imgView = UIImageView()
         imgView.backgroundColor = .purple
         imgView.widthAnchor.constraint(equalToConstant: 64).isActive = true
         imgView.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        imgView.leadingAnchor.constraint(equalTo: )
         imgView.layer.cornerRadius = 12;
         return imgView
     }()
@@ -42,12 +43,12 @@ class searchResultCell: UICollectionViewCell {
         button.setTitle("GET", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 10)
-        button.backgroundColor = UIColor.darkGray
+        button.backgroundColor = UIColor.lightGray
         button.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 32).isActive = true
         button.layer.borderColor = UIColor.black.cgColor
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 10
+        //button.layer.borderWidth = 1
+        button.layer.cornerRadius = 16
         button.clipsToBounds = true
         return button
     }()
@@ -55,14 +56,14 @@ class searchResultCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .lightGray
+        backgroundColor = .yellow
         
         let labelStackView  = UIStackView(arrangedSubviews: [
             nameLabel, categoryLabel, ratingLabel
             ])
             labelStackView.axis = .vertical
         let stackView = UIStackView(arrangedSubviews: [
-            imageView, labelStackView, getButton
+            appIconImagView, labelStackView, getButton
             ])
         stackView.spacing = 10
         stackView.alignment = .center
@@ -70,8 +71,8 @@ class searchResultCell: UICollectionViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
